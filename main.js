80754eecab092bc782c1903da38b995e8301aa55
ShahbajKhan <shktoky@gmail.com>
2022-03-09 17:46:22 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,63 @@
+document.getElementById('issueInputForm').addEventListener('submit', submitIssue);
+
+function submitIssue(e) {
+  const getInputValue = id => document.getElementById(id).value;
+  const description = getInputValue('issueDescription');
+  const severity = getInputValue('issueSeverity');
+  const assignedTo = getInputValue('issueAssignedTo');
+  const id = Math.floor(Math.random()*100000000) + '';
+  const status = 'Open';
+
+  const issue = { id, description, severity, assignedTo, status };
+  let issues = [];
+  if (localStorage.getItem('issues')){
+    issues = JSON.parse(localStorage.getItem('issues'));
+  }
+  issues.push(issue);
+  localStorage.setItem('issues', JSON.stringify(issues));
+
+  document.getElementById('issueInputForm').reset();
+  fetchIssues();
+  e.preventDefault();
+}
+
+const setStatusClosed = (event, id) => {
+  event.preventDefault()
+  const issues = JSON.parse(localStorage.getItem('issues'));
+  const currentIssue = issues.find(issue => issue.id == id);
+  currentIssue.status = 'Closed';
+  localStorage.setItem('issues', JSON.stringify(issues));
+  fetchIssues();
+  document.getElementById(`issueDetail${id}`).style.textDecoration = "line-through";
+}
+
+const deleteIssue = (e, id) => {
+  console.log(id)
+  const issues = JSON.parse(localStorage.getItem('issues'));
+  const remainingIssues = issues.filter(issue=> parseInt(issue.id) !== id )
+  console.log(remainingIssues)
+  localStorage.setItem('issues', JSON.stringify(remainingIssues));
+  fetchIssues()
+}
+
+const fetchIssues = () => {
+  const issues = JSON.parse(localStorage.getItem('issues'));
+  const issuesList = document.getElementById('issuesList');
+  issuesList.innerHTML = '';
+
+  for (var i = 0; i < issues.length; i++) {
+    const {id, description, severity, assignedTo, status} = issues[i];
+
+    issuesList.innerHTML +=   `<div class="well">
+                              <h6>Issue ID: ${id} </h6>
+                              <p><span class="label label-info"> ${status} </span></p>
+                              <h3 class="" id="issueDetail${id}"> ${description} </h3>
+                              <p><span class="glyphicon glyphicon-time"></span> ${severity}</p>
+                              <p><span class="glyphicon glyphicon-user"></span> ${assignedTo}</p>
+                              <button onclick="setStatusClosed(event, ${id})" class="btn btn-warning">Close</button>
+                              <button  onclick="deleteIssue(event, ${id})" class="btn btn-danger">Delete</button>
+                              </div>`;
+  }
+}
+
+
